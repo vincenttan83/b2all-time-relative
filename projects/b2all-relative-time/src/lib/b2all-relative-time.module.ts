0156f6b37bf7febb920377a957fc7c2c7bce2b18
vincenttan83 <vincenttan83@live.com>
2021-06-22 14:18:17 +0800

    library clean up

--- projects/b2all-relative-time/src/lib/b2all-relative-time.module.ts
+++ projects/b2all-relative-time/src/lib/b2all-relative-time.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { B2allRelativeTimeComponent } from './b2all-relative-time.component';
 import { DateAgoPipe } from './date-ago.pipe';
 import { DateDueInPipe } from './date-due-in.pipe';
 
@@ -7,14 +6,12 @@ import { DateDueInPipe } from './date-due-in.pipe';
 
 @NgModule({
   declarations: [
-    B2allRelativeTimeComponent,
     DateAgoPipe,
     DateDueInPipe
   ],
   imports: [
   ],
   exports: [
-    B2allRelativeTimeComponent,
     DateAgoPipe,
     DateDueInPipe,
   ]
--- projects/b2all-relative-time/src/public-api.ts
+++ projects/b2all-relative-time/src/public-api.ts
@@ -4,4 +4,6 @@
 
 // export * from './lib/b2all-relative-time.service';
 // export * from './lib/b2all-relative-time.component';
+export * from './lib/date-ago.pipe';
+export * from './lib/date-due-in.pipe';
 export * from './lib/b2all-relative-time.module';
